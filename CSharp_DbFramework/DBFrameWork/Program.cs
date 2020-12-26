ec7f8b143398378a655f2432cb9d4a0d369d2fcd
julyan97 <julyan997@gmail.com>
2020-12-26 12:50:24 +0200

    hidden conStr

--- CSharp_DbFramework/DBFrameWork/Program.cs
+++ CSharp_DbFramework/DBFrameWork/Program.cs
@@ -14,25 +14,30 @@ namespace DBFrameWork
     {
         static void Main(string[] args)
         {
-            //ApplicationDbContext db = new ApplicationDbContext(
-            //    "Server=DESKTOP-NVNDLO3\\SQLEXPRESS;Database=TestApp;Trusted_Connection=True;MultipleActiveResultSets=true");
-            //var user = new User("jojo", "23");
-            //db.Users.entities.Add(user);
-            //db.SaveChanges();
+            ApplicationDbContext db = new ApplicationDbContext(
+                "ConnectionString");
+            //TODO:add config file to read importnat info
 
-            var assemblies = Assembly.GetExecutingAssembly().GetTypes();
-            Console.WriteLine(assemblies);
-            assemblies.ToList().ForEach(x => Console.WriteLine(x.Name));
             var user = new User("jojo", "23");
-            var fields = user.GetType()
-                .GetFields(System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
 
-            foreach (var field in fields)
-            {
-                var atrs = field.GetCustomAttributes(false);
-                atrs.ToList().ForEach(a => Console.WriteLine(a.GetType().Name));
-            }
-          
+            db.Users.entities.Add(user);
+            db.SaveChanges();
+
+
+
+            //var assemblies = Assembly.GetExecutingAssembly().GetTypes();
+            //Console.WriteLine(assemblies);
+            //assemblies.ToList().ForEach(x => Console.WriteLine(x.Name));
+            //var user = new User("jojo", "23");
+            //var fields = user.GetType()
+            //    .GetFields(System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+            //foreach (var field in fields)
+            //{
+            //    var atrs = field.GetCustomAttributes(false);
+            //    atrs.ToList().ForEach(a => Console.WriteLine(a.GetType().Name));
+            //}
+
         }
     }
 }
