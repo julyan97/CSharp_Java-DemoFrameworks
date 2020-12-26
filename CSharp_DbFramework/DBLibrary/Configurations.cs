0bf66fbaf86c68df2327e7c2b6fb744dee78772f
julyan97 <julyan997@gmail.com>
2020-12-26 13:36:21 +0200

    Added configuration file for security reasons

---
+++ CSharp_DbFramework/DBLibrary/Configurations.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DBLibrary
+{
+    class Configurations
+    {
+        private dynamic settings;
+
+        public string this[string field] => settings[field];
+
+        public Configurations()
+        {
+            settings = Get();
+        }
+
+        private dynamic Get()
+        {
+            using StreamReader r = new StreamReader("config.json");
+            var json = r.ReadToEnd();
+            dynamic array = JsonConvert.DeserializeObject(json);
+            return array;
+        }
+    }
+}
